54c7fd901d9f451d3c001f83c078fff1c7ef7fc7
PonyCui <cuis@vip.qq.com>
2021-11-06 16:21:40 +0800

    refactor: Remove unused packages.

--- packages/mp_dom_runtime/packages/miniprogram_dom/tools/demo/pages/index/index.js
+++
@@ -1,9 +0,0 @@
-Page({
-  onLoad: function () {
-    var demoDom = this.selectComponent("#demoDom");
-    let document = demoDom.miniDom.document;
-    document.body.setStyle({
-      backgroundColor: "yellow",
-    });
-  },
-});
--- packages/mp_dom_runtime/packages/swan_dom/build.sh
+++
@@ -1,8 +0,0 @@
-tsc
-cp src/*.json dist/
-cp src/*.swan dist/
-cp src/*.css dist/
-cp src/*.js dist/
-
-cp dist/* ../../sample_swanapp/swan_dom/
-cp dist/* ../../dist_swan/swan_dom/
