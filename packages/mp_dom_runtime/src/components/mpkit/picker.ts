5a926dd23fe667f15e9b4ab599854dc01d13f88e
iLinQH <649995267@qq.com>
2021-11-18 23:57:54 +0800

    feat(picker): MPPicker support to show H5 picker

--- packages/mp_dom_runtime/src/components/mpkit/picker.ts
+++ packages/mp_dom_runtime/src/components/mpkit/picker.ts
@@ -1,16 +1,78 @@
+import { MPEnv } from "../..";
+import { PlatformType } from "../../env";
 import { setDOMAttribute } from "../dom_utils";
 import { MPPlatformView } from "../mpkit/platform_view";
-
+export class PickerItem {
+  constructor(readonly label: string, readonly disabled: boolean, readonly subItems: PickerItem[]) {}
+}
 export class MPPicker extends MPPlatformView {
+  weuiShadowRoot: any;
+  items: any;
+
   constructor(document: Document) {
     super(document);
+    if (MPEnv.platformType === PlatformType.browser) {
+      this.htmlElement.addEventListener("click", () => {
+        let shadowDiv = document.createElement("div");
+        document.body.appendChild(shadowDiv);
+        this.weuiShadowRoot = shadowDiv.attachShadow ? shadowDiv.attachShadow({ mode: "closed" }) : shadowDiv;
+        const script = document.createElement("script");
+        script.src = "https://res.wx.qq.com/open/libs/weuijs/1.2.1/weui.min.js";
+        document.body.appendChild(script);
+        const cssStyle = document.createElement("link");
+        cssStyle.rel = "stylesheet";
+        cssStyle.href = "https://cdn.jsdelivr.net/npm/weui@2.4.4/dist/style/weui.min.css";
+        this.weuiShadowRoot.appendChild(cssStyle);
+        const div = document.createElement("body");
+        div.setAttribute("data-weui-theme", "light");
+        div.style.position = "absolute";
+        div.style.width = "100%";
+        div.style.height = "100%";
+        this.weuiShadowRoot.appendChild(div);
+        (window as any).weui.picker(
+          (this.attributes.items as PickerItem[])?.map((it, idx) => {
+            return {
+              label: it.label,
+              value: idx,
+              disabled: it.disabled,
+              children: it.subItems?.map((it, idx) => {
+                return {
+                  label: it.label,
+                  value: idx,
+                  disabled: it.disabled,
+                  children: it.subItems?.map((it, idx) => {
+                    return { label: it.label, value: idx, disabled: it.disabled };
+                  }),
+                };
+              }),
+            };
+          }),
+          {
+            container: div,
+            onConfirm: function (result: any) {
+              console.log(result);
+              this.invokeMethod("onChangeCallback", { result: result });
+            },
+            onClose: function () {
+              div.remove();
+            },
+          }
+        );
+      });
+    }
     this.htmlElement.addEventListener("change", (e: any) => {
       this.invokeMethod("onChangeCallback", { type: e.type, detail: e.detail });
     });
   }
 
   elementType() {
-    return "wx-picker";
+    if (MPEnv.platformType === PlatformType.browser) {
+      return "div";
+    } else if (MPEnv.platformType === PlatformType.wxMiniProgram) {
+      return "wx-picker";
+    } else {
+      return "";
+    }
   }
 
   setAttributes(attributes: any) {
--- packages/mpcore/lib/mpkit/picker.dart
+++ packages/mpcore/lib/mpkit/picker.dart
@@ -25,44 +25,23 @@ class MPPicker extends MPPlatformView {
 
   MPPicker({
     required Widget child,
+    List<PickerItem>? items,
     String? headerText,
     MPPickerMode? mode,
     bool? disabled,
     MPPickerController? controller,
     this.onChangeCallback,
   }) : super(
-          viewType: "mp_picker",
+          viewType: 'mp_picker',
           viewAttributes: {
             'headerText': headerText,
             'mode': mode?.toString(),
             'disabled': disabled,
+            'items': items,
           }..removeWhere((key, value) => value == null),
           child: child,
           controller: controller,
         ) {
-    // if (MPEnv.envHost() == MPEnvHostType.browser) {
-    //   child = GestureDetector(
-    //     onTap: () async {
-    //       final result = await MPAction(
-    //         type: 'web_dialogs',
-    //         params: {
-    //           'dialogType': 'picker',
-    //           'title': 'title',
-    //           'items': [
-    //             PickerItem(label: '飞机票'),
-    //             PickerItem(label: '火车票'),
-    //             PickerItem(label: '的士票'),
-    //             PickerItem(label: '公交票 (disabled)', disabled: true),
-    //             PickerItem(label: '其他'),
-    //           ],
-    //           'confirmText': 'confirmText',
-    //         },
-    //       ).send();
-    //       final _ = controller?.onMethodCall('onChangeCallback', result);
-    //     },
-    //     child: child,
-    //   );
-    // }
     if (onChangeCallback != null) {
       assert(
         controller != null,
