fd52eeba6dca31e3f1acb7dcf8afd011843bf2f9
iLinQH <649995267@qq.com>
2021-11-27 12:36:41 +0800

    feat(slider): MPSlider support to set min/max/step/disabled

--- packages/mp_dom_runtime/src/components/mpkit/slider.ts
+++ packages/mp_dom_runtime/src/components/mpkit/slider.ts
@@ -8,17 +8,10 @@ export class MPSlider extends MPPlatformView {
   sliderElement: any;
   constructor(document: Document) {
     super(document);
-    if (MPEnv.platformType === PlatformType.wxMiniProgram) {
-      // this.htmlElement.addEventListener("change", (e: any) => {
-      //   this.invokeMethod("onSliderChange", { value: e.detail.value });
-      // });
-      this.htmlElement.addEventListener("changing", (e: any) => {
-        this.invokeMethod("onSliderChanging", { value: e.detail.value });
-      });
-    } else if (MPEnv.platformType === PlatformType.browser) {
-      this.htmlElement.addEventListener("change", (value: any) => {
-        this.invokeMethod("onSliderChanging", { value: value.detail.value });
-      });
+    this.htmlElement.addEventListener("change", (value: any) => {
+      this.invokeMethod("onValueChanged", { value: value.detail.value });
+    });
+    if (MPEnv.platformType === PlatformType.browser) {
       const weuiShadowRoot = this.htmlElement.attachShadow
         ? this.htmlElement.attachShadow({ mode: "closed" })
         : this.htmlElement;
@@ -26,32 +19,9 @@ export class MPSlider extends MPPlatformView {
       cssStyle.rel = "stylesheet";
       cssStyle.href = "https://cdn.jsdelivr.net/npm/weui@2.4.4/dist/style/weui.min.css";
       weuiShadowRoot.appendChild(cssStyle);
-
       this.sliderElement = document.createElement("body");
       this.sliderElement.setAttribute("data-weui-theme", "light");
-
       weuiShadowRoot.appendChild(this.sliderElement);
-      setTimeout(() => {
-        var totalLen = this.sliderElement.querySelector("#sliderInner")!.clientWidth,
-          startLeft = 0,
-          startX = 0;
-        var sliderTrack = this.sliderElement.querySelector("#sliderTrack") as HTMLDivElement;
-        var sliderHandler = this.sliderElement.querySelector("#sliderHandler") as HTMLDivElement;
-        sliderHandler.addEventListener("touchstart", (e: any) => {
-          startLeft = (parseInt(sliderHandler.style.left) * totalLen) / 100;
-          startX = e.changedTouches[0].clientX;
-        });
-        sliderHandler.addEventListener("touchmove", (e: any) => {
-          var dist = startLeft + e.changedTouches[0].clientX - startX,
-            percent;
-          dist = dist < 0 ? 0 : dist > totalLen ? totalLen : dist;
-          percent = (dist / totalLen) * 100;
-          sliderTrack.style.width = percent + "%";
-          sliderHandler.style.left = percent + "%";
-          this.htmlElement.dispatchEvent(new CustomEvent("change", { detail: { value: percent } } as any));
-          e.preventDefault();
-        });
-      }, 300);
     }
   }
 
@@ -69,19 +39,6 @@ export class MPSlider extends MPPlatformView {
     setDOMAttribute(this.htmlElement, "step", attributes.step);
     setDOMAttribute(this.htmlElement, "disabled", attributes.disabled);
     setDOMAttribute(this.htmlElement, "value", attributes.value);
-    setDOMAttribute(
-      this.htmlElement,
-      "active-color",
-      attributes.activeColor ? cssColorHex(attributes.activeColor) : null
-    );
-    setDOMAttribute(
-      this.htmlElement,
-      "background-color",
-      attributes.backgroundColor ? cssColorHex(attributes.backgroundColor) : null
-    );
-    setDOMAttribute(this.htmlElement, "block-size", attributes.blockSize);
-    setDOMAttribute(this.htmlElement, "block-color", attributes.blockColor ? cssColorHex(attributes.blockColor) : null);
-    setDOMAttribute(this.htmlElement, "show-value", attributes.showValue);
     if (MPEnv.platformType === PlatformType.browser && this.firstSetup) {
       this.firstSetup = false;
       this.sliderElement.innerHTML = `<div class="weui-slider">
@@ -94,6 +51,34 @@ export class MPSlider extends MPPlatformView {
           }%;" class="weui-slider__handler weui-wa-hotarea"></div>
         </div>
       </div>`;
+      if (attributes.disabled !== true) {
+        const min = this.attributes.min ?? 0;
+        const max = this.attributes.max ?? 100;
+        const step = (((this.attributes.step ?? 1) * 1.0) / (max - min)) * 100;
+        setTimeout(() => {
+          var totalLen = this.sliderElement.querySelector("#sliderInner")!.clientWidth,
+            startLeft = 0,
+            startX = 0;
+          var sliderTrack = this.sliderElement.querySelector("#sliderTrack") as HTMLDivElement;
+          var sliderHandler = this.sliderElement.querySelector("#sliderHandler") as HTMLDivElement;
+          sliderHandler.addEventListener("touchstart", (e: any) => {
+            startLeft = (parseInt(sliderHandler.style.left) * totalLen) / 100;
+            startX = e.changedTouches[0].clientX;
+          });
+          sliderHandler.addEventListener("touchmove", (e: any) => {
+            var dist = startLeft + e.changedTouches[0].clientX - startX,
+              percent;
+            dist = dist < 0 ? 0 : dist > totalLen ? totalLen : dist;
+            percent = Math.round(((dist / totalLen) * 100) / step) * step;
+            sliderTrack.style.width = percent + "%";
+            sliderHandler.style.left = percent + "%";
+            this.htmlElement.dispatchEvent(
+              new CustomEvent("change", { detail: { value: ((max - min) * percent) / 100 } } as any)
+            );
+            e.preventDefault();
+          });
+        }, 300);
+      }
     }
   }
 }
--- packages/mpcore/lib/mpkit/slider.dart
+++ packages/mpcore/lib/mpkit/slider.dart
@@ -1,25 +1,22 @@
 part of 'mpkit.dart';
 
 class MPSlider extends StatelessWidget {
+  final int min;
+  final int max;
+  final int step;
+  final bool disabled;
   final int width;
-  final int? value;
-  final Function(Map)? onSliderChange;
-  final Function(Map)? onSliderChanging;
+  final int value;
+  final Function(Map)? onValueChanged;
 
   MPSlider({
-    // int? min,
-    // int? max,
-    // int? step,
-    // bool? disabled,
+    this.min = 0,
+    this.max = 100,
+    this.step = 1,
+    this.disabled = false,
     this.width = 300,
-    this.value,
-    // Color? activeColor,
-    // Color? backgroundColor,
-    // int? blockSize,
-    // Color? blockColor,
-    // bool? showValue,
-    this.onSliderChange,
-    this.onSliderChanging,
+    this.value = 0,
+    this.onValueChanged,
   });
 
   @override
@@ -31,24 +28,15 @@ class MPSlider extends StatelessWidget {
       child: MPPlatformView(
         viewType: 'mp_slider',
         viewAttributes: {
-          // 'min': min,
-          // 'max': max,
-          // 'step': step,
-          // 'disabled': disabled,
+          'min': min,
+          'max': max,
+          'step': step,
+          'disabled': disabled,
           'value': value,
-          // 'activeColor':
-          //     activeColor == null ? null : activeColor.value.toString(),
-          // 'backgroundColor':
-          //     backgroundColor == null ? null : backgroundColor.value.toString(),
-          // 'blockSize': blockSize,
-          // 'blockColor': blockColor == null ? null : blockColor.value.toString(),
-          // 'showValue': showValue,
         }..removeWhere((key, value) => value == null),
         onMethodCall: (method, args) {
-          if (method == 'onSliderChange' && args is Map) {
-            onSliderChange?.call(args);
-          } else if (method == 'onSliderChanging' && args is Map) {
-            onSliderChanging?.call(args);
+          if (method == 'onValueChanged' && args is Map) {
+            onValueChanged?.call(args);
           }
         },
       ),
