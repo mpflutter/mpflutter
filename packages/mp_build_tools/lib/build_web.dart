783d6dbf4a9232922b181cd9e160cc03027f9b9a
PonyCui <cuis@vip.qq.com>
2021-12-16 15:57:59 +0800

    fix: The flutter build bundle not response issue.

--- packages/mp_build_tools/lib/build_web.dart
+++ packages/mp_build_tools/lib/build_web.dart
@@ -60,6 +60,7 @@ void _buildDartJS(List<String> args) {
       'bundle',
     ],
     runInShell: true,
+    environment: {'PUB_HOSTED_URL': 'https://pub.mpflutter.com'},
   );
   if (buildBundleResult.exitCode != 0) {
     print(buildBundleResult.stdout);
