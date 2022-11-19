dc3d659345256300b61a7df1c620eabac882d7d8
崔 明辉 <cuis@vip.qq.com>
2022-11-19 22:14:34 +0800

    refactor: 删除 tt 及 swan 文件夹。

--- packages/mpflutter_sample/swanapp/README.md
+++
@@ -1,3 +0,0 @@
-# swan hello world 
-
-开发者工具新建工程,生成demo
--- packages/mpflutter_sample/swanapp/pages/index/index.js
+++
@@ -1,7 +0,0 @@
-// index.js
-// 获取应用实例
-const WXPage = require('../../mpdom.min').WXPage;
-
-const thePage = new WXPage;
-thePage.kboneRender = require('../../kbone/miniprogram-render/index')
-Page(thePage);
