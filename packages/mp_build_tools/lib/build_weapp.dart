df96910ac800082a4cc04a43079d7523fde5c68d
崔 明辉 <cuis@vip.qq.com>
2023-02-19 12:26:14 +0800

    feat: genRandomValues 添加伪随机数逻辑。

--- packages/mp_build_tools/lib/build_weapp.dart
+++ packages/mp_build_tools/lib/build_weapp.dart
@@ -343,12 +343,22 @@ _fixCryptoRequire() {
 let lastRandomValues = undefined;
 
 export const genRandomValues = () => {
-  wx.getRandomValues({
-    length: 32,
-    success: (res) => {
-      lastRandomValues = new Uint8Array(res.randomValues);
+  if (wx && wx.getRandomValues) {
+    wx.getRandomValues({
+      length: 32,
+      success: (res) => {
+        lastRandomValues = new Uint8Array(res.randomValues);
+      }
+    })
+  }
+  else {
+    console.warn("不存在 wx.getRandomValues 接口，正在使用伪随机数。");
+    let arr = [];
+    for (let index = 0; index < 32; index++) {
+      arr.push(Math.floor(Math.random() * 255));
     }
-  })
+    lastRandomValues = new Uint8Array(arr);
+  }
 }
 
 export const randomFillSync = (buffer) => {
