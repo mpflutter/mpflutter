c178f6e3c67e5144b806c85542290907fa89ae4e
白自好 <644795178@qq.com>
2023-04-05 18:02:11 +0800

    完善微信小游戏模版

--- packages/mpflutter_sample/minigame/game.js
+++ packages/mpflutter_sample/minigame/game.js
@@ -1,16 +1,34 @@
-const canvas = wx.createCanvas()
+import './js/libs/weapp-adapter'
+import './js/libs/symbol'
+
 const ctx = canvas.getContext('2d')
 
-class Main {
+export default class Main {
 
   constructor() {
     this.restart();
   }
 
   restart() {
-    ctx.clearRect(0,0,1000,1000);
-    ctx.fillStyle = "red";
-    ctx.fillRect(0,0,100,100);
+
+    this.aniId = 0
+    window.mpDEBUG = true;
+    const { Engine, Page, CanvasApp } = require("./js/mpdom.min");
+
+    var engine = new Engine();
+    var dev = true;
+    if (dev) {
+      // engine.initWithDebuggerServerAddr(new URL(window.location.href).hostname + ':9898');
+      engine.initWithDebuggerServerAddr('127.0.0.1:9898');
+      engine.start();
+    } else {
+      Engine.codeBlockWithCodePath('main.dart.js').then(function (codeBlock) {
+        engine.initWithCodeBlock(codeBlock);
+        engine.start();
+      });
+    }
+    var app = new CanvasApp(ctx, engine);
+    app.setupFirstPage();
   }
 
 }
