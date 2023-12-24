d38a005a3c6deac3812979367820924f668ace12
崔 明辉 <cuis@vip.qq.com>
2023-12-24 11:51:04 +0800

    feat: add coonnect host tips

--- lib/dev_app/runApp.dart
+++ lib/dev_app/runApp.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:mpflutter_core/dev_app/dev_server.dart';
@@ -27,46 +29,96 @@ class _MPAppState extends State<MPApp> {
     });
   }
 
-  Widget renderDisconnected() {
-    return Container(
-      color: Colors.blue,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+  @override
+  Widget build(BuildContext context) {
+    if (!IsolateDevServer.shared.connected()) {
+      return ConnectHostTips();
+    }
+    return widget.child;
+  }
+}
+
+class ConnectHostTips extends StatefulWidget {
+  const ConnectHostTips({
+    super.key,
+  });
+
+  @override
+  State<ConnectHostTips> createState() => _ConnectHostTipsState();
+}
+
+class _ConnectHostTipsState extends State<ConnectHostTips> {
+  int currentStep = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      home: Scaffold(
+        body: ListView(
           children: [
-            Text(
-              '未能连接到调试宿主',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.yellow,
-                fontWeight: FontWeight.bold,
+            SizedBox(height: 22),
+            ListTile(
+              title: Align(
+                alignment: Alignment.centerLeft,
+                child: Icon(
+                  Icons.connect_without_contact,
+                  size: 48,
+                  color: Colors.grey,
+                ),
               ),
-              textDirection: TextDirection.ltr,
-              textAlign: TextAlign.center,
             ),
-            SizedBox(height: 12),
-            Text(
-              '应用将在宿主连接后恢复',
-              style: TextStyle(
-                fontSize: 12,
-                color: Colors.yellow.withOpacity(0.8),
-                fontWeight: FontWeight.bold,
+            ListTile(
+              title: Text(
+                "未连接到调试宿主",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
               ),
-              textDirection: TextDirection.ltr,
-              textAlign: TextAlign.center,
+            ),
+            Divider(
+              color: Colors.black.withOpacity(0.05),
+              indent: 16,
+              endIndent: 16,
+            ),
+            ListTile(title: Text('宿主连接方法')),
+            Stepper(
+              currentStep: currentStep,
+              onStepContinue: () {
+                setState(() {
+                  currentStep = min(3, currentStep + 1);
+                });
+              },
+              onStepCancel: () {
+                setState(() {
+                  currentStep = max(0, currentStep - 1);
+                });
+              },
+              steps: [
+                Step(
+                  title: Text("构建 Dev Mode 产物"),
+                  content:
+                      Text('使用 dart scripts/build_wechat.dart --devmode 构建应用。'),
+                ),
+                Step(
+                  title: Text("导入产物"),
+                  content: Text(
+                      '使用微信开发者工具，导入产物。                                       '),
+                ),
+                Step(
+                  title: Text("运行产物"),
+                  content: Text(
+                      '微信开发者工具，直接在模拟器上运行，或者在微信扫码预览。                                       '),
+                ),
+                Step(
+                  title: Text("注意"),
+                  content: Text(
+                      '不要同时在模拟器和真机上预览，请关掉其中一个。                                       '),
+                ),
+              ],
             ),
           ],
         ),
       ),
     );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (!IsolateDevServer.shared.connected()) {
-      return renderDisconnected();
-    }
-    return widget.child;
   }
 }
 
--- lib/mpflutter_core.dart
+++ lib/mpflutter_core.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:window_manager/window_manager.dart';
 import './dev_app/runApp.dart' if (dart.library.js) './wechat_app/runApp.dart';
 
 export './logger.dart';
@@ -14,8 +15,19 @@ const bool kIsMPFlutterDevmode = bool.fromEnvironment(
   defaultValue: false,
 );
 
-void runMPApp(Widget app) {
+void runMPApp(Widget app) async {
   if (kIsMPFlutter) {
+    if (kIsMPFlutterDevmode) {
+      WidgetsFlutterBinding.ensureInitialized();
+      await windowManager.ensureInitialized();
+      WindowOptions windowOptions = WindowOptions(
+        size: Size(414, 896),
+      );
+      windowManager.waitUntilReadyToShow(windowOptions, () async {
+        await windowManager.show();
+        await windowManager.focus();
+      });
+    }
     runApp(MPApp(child: app));
   } else {
     runApp(app);
