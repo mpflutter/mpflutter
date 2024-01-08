c32433d9a89bfc567b24ef26f96bf615e3309a39
崔 明辉 <cuis@vip.qq.com>
2024-01-08 16:37:53 +0800

    fix: 启动过程可能有一闪而过的黑屏

--- lib/mpflutter_splash.dart
+++ lib/mpflutter_splash.dart
@@ -2,10 +2,15 @@
 // Use of this source code is governed by a Apache License Version 2.0 that can be
 // found in the LICENSE file.
 
+import 'dart:async';
+
 import 'mpjs/mpjs.dart' as mpjs;
 
 class MPFlutterSplashManager {
+  static bool _displayingSplash = true;
+
   static void displaySplash() {
+    _displayingSplash = true;
     mpjs.JSObject self = mpjs.context["FlutterHostView"]["shared"]["self"];
     self.callMethod("setData", [
       {
@@ -15,11 +20,15 @@ class MPFlutterSplashManager {
   }
 
   static void hideSplash() {
-    mpjs.JSObject self = mpjs.context["FlutterHostView"]["shared"]["self"];
-    self.callMethod("setData", [
-      {
-        "readyToDisplay": true,
-      }
-    ]);
+    _displayingSplash = false;
+    Timer(Duration(milliseconds: 500), () {
+      if (_displayingSplash) return;
+      mpjs.JSObject self = mpjs.context["FlutterHostView"]["shared"]["self"];
+      self.callMethod("setData", [
+        {
+          "readyToDisplay": true,
+        }
+      ]);
+    });
   }
 }
