4a6fcd85160a8d49071413703536546315ba1716
崔 明辉 <cuis@vip.qq.com>
2024-01-14 17:07:56 +0800

    fix: MPFlutterDarkmodeManager 添加 kIsMPFlutter 判断

--- lib/mpflutter_darkmode.dart
+++ lib/mpflutter_darkmode.dart
@@ -2,16 +2,24 @@
 // Use of this source code is governed by a Apache License Version 2.0 that can be
 // found in the LICENSE file.
 
+import 'package:mpflutter_core/mpflutter_core.dart';
+
 import 'mpjs/mpjs.dart' as mpjs;
 
 class MPFlutterDarkmodeManager {
   static bool isDarkmode() {
+    if (!kIsMPFlutter) {
+      return false;
+    }
     mpjs.JSObject wx = mpjs.context["wx"];
     final result = wx.callMethod("getSystemInfoSync", [{}]);
     return result["theme"] == "dark";
   }
 
   static void addThemeListener(Function() callback) {
+    if (!kIsMPFlutter) {
+      return;
+    }
     mpjs.JSObject wx = mpjs.context["wx"];
     wx.callMethod("onThemeChange", [
       (_) {
