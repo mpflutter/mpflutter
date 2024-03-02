f2a6306ea7e3267b2105e16a8db975803f7764d5
崔 明辉 <cuis@vip.qq.com>
2024-03-02 21:36:33 +0800

    feat: 添加 MPFlutterImageEncoder.encodeToBase64 和 MPFlutterImageEncoder.encodeToFilePath 方法。

--- lib/mpflutter_image_encoder.dart
+++ lib/mpflutter_image_encoder.dart
@@ -11,6 +11,8 @@ enum MPFlutterImageByteFormat {
 }
 
 class MPFlutterImageEncoder {
+  /// 不建议使用该方法，该方法使用 base64.decode 会导致严重卡顿。
+  /// 建议使用 encodeToFilePath 或 encodeToBase64
   static Future<ByteData?> encodeToBytes({
     required ui.Image image,
     MPFlutterImageByteFormat format = MPFlutterImageByteFormat.png,
@@ -32,6 +34,61 @@ class MPFlutterImageEncoder {
         } catch (e) {
           completer.complete(null);
         }
+      }
+    ]);
+    return completer.future;
+  }
+
+  /// 保存 Image 成 Base64 字符串
+  static Future<String?> encodeToBase64({
+    required ui.Image image,
+    MPFlutterImageByteFormat format = MPFlutterImageByteFormat.png,
+    double compressQuality = 0.92,
+  }) async {
+    final rawRgba = await image.toByteData(format: ui.ImageByteFormat.rawRgba);
+    if (rawRgba == null) return null;
+    final completer = Completer<String?>();
+    (mpjs.context["encodeImage"] as mpjs.JSFunction).call([
+      rawRgba.buffer.asUint8List(),
+      image.width,
+      image.height,
+      "image/${format.name}",
+      compressQuality,
+      (String result) {
+        try {
+          completer.complete(result.split("base64,")[1]);
+        } catch (e) {
+          completer.complete(null);
+        }
+      }
+    ]);
+    return completer.future;
+  }
+
+  /// 直接将 Image 保存成小程序文件，然后你可以使用 WX API 上传到服务器，或使用 WX API 保存到用户相册。
+  static Future<String> encodeToFilePath({
+    required ui.Image image,
+    required String filePath,
+    MPFlutterImageByteFormat format = MPFlutterImageByteFormat.png,
+    double compressQuality = 0.92,
+  }) async {
+    final rawRgba = await image.toByteData(format: ui.ImageByteFormat.rawRgba);
+    if (rawRgba == null) {
+      throw "no raw rgba data";
+    }
+    final completer = Completer<String>();
+    (mpjs.context["encodeImageToFilePath"] as mpjs.JSFunction).call([
+      rawRgba.buffer.asUint8List(),
+      image.width,
+      image.height,
+      "image/${format.name}",
+      compressQuality,
+      filePath,
+      (String filePath) {
+        if (filePath.isEmpty) {
+          throw "save file failed";
+        }
+        completer.complete(filePath);
       }
     ]);
     return completer.future;
